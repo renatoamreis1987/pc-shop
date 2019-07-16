62f7babeaccb764b10a2162c36b6a589ee13ccf0
Renato Reis <email@renatoamreis.com>
2019-07-16 16:57:08 +0200

    Adding files to the project

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,14 +49,12 @@ class Directory extends React.Component {
 
     render() {
         return (
-            <div className="directory-menu">
-                {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
-                    ))
-                }
-            </div>
-        )
+          <div className="directory-menu">
+            {this.state.sections.map(({ id, ...otherSectionProps }) => (
+              <MenuItem key={id} {...otherSectionProps} />
+            ))}
+          </div>
+        );
     }
 }
 
